fb2d54a22478ff30e19506b29c90248fbd927727
Luís Vinicius Capelletto <thecapellett@gmail.com>
2021-05-27 03:26:05 -0300

    Feat: Avoid duplicated Stripe Customers

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,18 +1,51 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/client";
+import { query as q } from "faunadb";
+
 import { stripe } from "../../services/stripe";
+import { fauna } from "../../services/fauna";
+
+type User = {
+  ref: {
+    id: string;
+  };
+  data: {
+    stripe_customer_id: string;
+  };
+};
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
     const session = await getSession({ req });
 
-    const stripeCustomer = await stripe.customers.create({
-      email: session.user.email,
-      //metadata
-    });
+    // procura por usuario no FaunaDB
+    const user = await fauna.query<User>(
+      q.Get(q.Match(q.Index("user_by_email"), q.Casefold(session.user.email)))
+    );
+
+    let customerId = user.data.stripe_customer_id;
+
+    if (!customerId) {
+      // Cria novo cliente
+      const stripeCustomer = await stripe.customers.create({
+        email: session.user.email,
+        // metadata
+      });
+
+      // seta um novo indice para clientes no FaunaDB
+      await fauna.query(
+        q.Update(q.Ref(q.Collection("users"), user.ref.id), {
+          data: {
+            stripe_customer_id: stripeCustomer.id,
+          },
+        })
+      );
+
+      customerId = stripeCustomer.id;
+    }
 
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
-      customer: stripeCustomer.id,
+      customer: customerId,
       payment_method_types: ["card"],
       billing_address_collection: "required",
       line_items: [{ price: "price_1IvGyRDTU4NemoJJWGcNq1L1", quantity: 1 }],
