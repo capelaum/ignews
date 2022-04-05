e78df669e0b34d00a03ba6f7743b056dee761633
Luís Vinicius Capelletto <thecapellett@gmail.com>
2022-04-05 04:55:00 -0300

    ✅ Home page tests

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ import styles from './home.module.scss'
 interface HomeProps {
   product: {
     priceId: string
-    amount: number
+    amount: string
   }
 }
 
@@ -26,9 +26,9 @@ export default function Home({ product }: HomeProps) {
           </h1>
           <p>
             Get access to all the publications <br />
-            <span>for {product.amount} month</span>
+            <span>for {product.amount} monthly</span>
           </p>
-          <SubscribeButton priceId={product.priceId} />
+          <SubscribeButton />
         </section>
 
         <img src='/images/avatar.svg' alt='Girl coding' />
